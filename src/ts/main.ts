2c07fbe49d70e9ff6d3dcb0d1297234b2a50b220
kayak <kayak@kayak.rocks>
2021-01-09 14:26:21 +1000

    🚧 (pixi): add pixi stage and make it resize properly
    closes #69

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,32 @@
-if (process.env.NODE_ENV === 'development')
-  console.log('TypeScript compiled to JavaScript.')
+import * as PIXI from 'pixi.js'
+
+const htmlContainer = document.getElementById('pixi-container') as HTMLElement
+
+// the following taken from one of the pixi demos lol
+
+const app = new PIXI.Application({
+  backgroundColor: 0x1099bb,
+  resizeTo: htmlContainer,
+})
+
+htmlContainer.appendChild(app.view)
+
+// create a new Sprite from an image path
+const bunny = PIXI.Sprite.from('../img/header.png')
+
+// center the sprite's anchor point
+bunny.anchor.set(0.5)
+
+// move the sprite to the center of the screen
+bunny.x = app.screen.width / 2
+bunny.y = app.screen.height / 2
+
+app.stage.addChild(bunny)
+
+// recentre sprite on window resize
+// TODO: add throttling or risk melting my devices
+window.onresize = () => {
+  console.log('window resized')
+  bunny.x = app.screen.width / 2
+  bunny.y = app.screen.height / 2
+}
